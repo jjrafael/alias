1e5539e0924d43c56e6adedd7955a00a253b31c0
jjrafael <jessiejamesrafael@gmail.com>
2019-08-07 15:41:09 +0800

    updated serviceWorker to latest copy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reducers from './reducers';
 import api from './middleware/api';
-import registerServiceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers =
 typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -29,4 +29,4 @@ document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-registerServiceWorker();
+serviceWorker.unregister();
