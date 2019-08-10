78ddd98f3707ad52b4092adff65f1beea0a1f423
jjrafael <jessiejamesrafael@gmail.com>
2019-08-10 12:14:22 +0800

    Updated cache verification logic

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -13,7 +13,7 @@ import Loading from './common/Loading';
 
 //actions
 import { setDeviceDetails, readApp, readUser, toggleLoadingOverlay } from '../actions/app';
-import { setGame } from '../actions/games';
+import { readGame } from '../actions/games';
 
 //misc
 import { isMobile } from '../utils/app';
@@ -35,9 +35,9 @@ const mapDispatchToProps = dispatch => {
   return bindActionCreators(
     {
       setDeviceDetails,
-      setGame,
-      readApp,
       toggleLoadingOverlay,
+      readGame,
+      readApp,
       readUser
     },
     dispatch
@@ -51,47 +51,32 @@ class MainView extends React.Component {
 			device: null,
 			cachedGameChecked: false,
 			cachedAppChecked: false,
+			cachedUserChecked: false,
+			cacheLoaded: false,
+			verifyCacheDone: false,
+			cachedIds: null,
 		}
 	}
 
 	componentDidMount() {
 		const navigator = window.navigator;
-		const { gameId, appId } = getAllLocalStorage();
+		const { gameId, appId, userId } = getAllLocalStorage();
 		const device = {
 			device: isMobile(navigator.userAgent) || 'desktop',
 			platform: navigator.platform,
 			browser: navigator.appCodeName,
 		}
 		this.props.setDeviceDetails(device);
-		this.setState({device});
-			
-		//check if there's cached active app
-		if(appId){
-			this.props.toggleLoadingOverlay(true, 'Verifying Cache..');
-			this.checkActiveApp(appId);
-
-			//check if there's cached active game
-			if(gameId){
-				this.checkActiveGame(gameId);
-			}else{
-				this.setState({cachedGameChecked: true});
-			}
-		}else{
-			this.setState({cachedAppChecked: true});
-		}
+		this.setState({ device, cachedIds: { gameId, appId, userId } });
 	}
 
-	componentDidUpdate(prevProps) {
+	componentDidUpdate(prevProps, prevState) {
+		if(prevState.cachedIds !== this.state.cachedIds && this.state.cachedIds){
+			this.checkActiveUser();
+		}
+
 		if(prevProps.appInitializing !== this.props.appInitializing){
 			this.props.toggleLoadingOverlay(this.props.appInitializing, 'Initializing App');
-		}
-
-		if(prevProps.readingApp !== this.props.readingApp && !this.props.readingApp){
-			this.closeLoading();
-		}
-
-		if(prevProps.readingUser !== this.props.readingUser && !this.props.readingUser){
-			this.closeLoading();
 		}
 
 		if(prevProps.appDetails !== this.props.appDetails && !!this.props.appDetails){
@@ -108,35 +93,104 @@ class MainView extends React.Component {
 		this.props.toggleLoadingOverlay();
 	}
 
-	checkActiveApp(id) {
-		let app = null;
-
-		this.props.readApp(id).then((doc) => {
-			app = doc.response.data();
-			//if exists and active
-			if(doc.response.exists && app.status === 'active'){
-				this.setState({cachedAppChecked: true});
-			}
-		})
+	updateLoading(data){
+		//update attributes of loading overlay
+		this.props.toggleLoadingOverlay(true, data);
 	}
 
-	checkActiveGame(id) {
-		const game = null;
+	checkActiveUser() {
+		//check if there's logged user
+		const { userId } = this.state.cachedIds;
+		const doneStates = {
+			cachedGameChecked: true,
+			cachedAppChecked: true,
+			cachedUserChecked: true,
+			cacheLoaded: false,
+			verifyCacheDone: true
+		}
+		let user = null;
 
-		if(game){
-			this.props.setGame({...game, id});
-			this.setState({cachedGameChecked: true});
+		if(userId){
+			this.updateLoading('Verifying Cache...');
+			this.props.readUser(userId).then((doc) => {
+				user = doc.response.data();
+				//if exists and active
+				if(doc.response.exists && user.status === 'active' && !doc.error){
+					this.checkActiveApp();
+				}else{
+					this.closeLoading();
+					this.setState({ ...doneStates });
+				}
+			})
+		}else{
+			this.closeLoading();
+			this.setState({ ...doneStates });
+		}
+	}
+
+	checkActiveApp() {
+		//check if there's cached active app
+		const { appId } = this.state.cachedIds;
+		const doneStates = {
+			cachedGameChecked: true,
+			cachedAppChecked: true,
+			cachedUserChecked: true,
+			cacheLoaded: true,
+			verifyCacheDone: true
+		}
+		let app = null;
+
+		if(appId){
+			this.updateLoading('Initializing App...');
+			this.props.readApp(appId).then((doc) => {
+				app = doc.response.data();
+				//if exists and active
+				if(doc.response.exists && app.status === 'active' && !doc.error){
+					this.setState({cachedAppChecked: true, cacheLoaded: true});
+					this.checkActiveGame();
+				}else{
+					this.closeLoading();
+					this.setState({ ...doneStates });
+				}
+			})
+		}else{
+			this.closeLoading();
+			this.setState({ ...doneStates });
+		}
+	}
+
+	checkActiveGame() {
+		//check if there's cached unfinished game
+		const { gameId } = this.state.cachedIds;
+		const doneStates = {
+			cachedGameChecked: true,
+			cachedAppChecked: true,
+			cachedUserChecked: true,
+			cacheLoaded: true,
+			verifyCacheDone: true
+		}
+
+		if(gameId){
+			this.updateLoading('In a moment...');
+			this.props.readGame(gameId).then((doc) => {
+				//if exists and active
+				this.closeLoading();
+				this.setState({ ...doneStates });
+			})
+		}else{
+			this.closeLoading();
+			this.setState({ ...doneStates });
 		}
 	}
 
   	render() {
   		const { user, appDetails } = this.props;
-  		const { cachedAppChecked } = this.state;
+  		const { verifyCacheDone } = this.state;
   		const props = { variables };
   		const isLogged = user && user.is_logged;
-  		const isAppReady = (isLogged && !!appDetails && cachedAppChecked);
-  		// const isGameReady = isAppReady && cachedGameChecked;
-  		const isTeamReady = false;
+  		const isAppReady = (isLogged && !!appDetails && verifyCacheDone);
+  		const isTeamReady = isAppReady && false;
+  		const isGridReady = isAppReady && false;
 
 	    return (
 	      <div className="page" id="MainPage">
@@ -144,7 +198,7 @@ class MainView extends React.Component {
 	        <Body className="app-body">
 	        	<Switch>
 	        		<Route exact path="/"  render={() => isAppReady ? <HomePage {...props} /> : <SplashPage {...props} /> }/>
-	        		<Route exact path="/home"render={() => isAppReady ? <HomePage {...props} /> : <SplashPage {...props} /> }/>
+	        		<Route exact path="/grid" render={() => isGridReady ? <HomePage {...props} /> : <SplashPage {...props} /> }/>
 	        		<Route exact path="/build-team" render={() => isTeamReady ? <BuildTeamPage {...props} /> : <SplashPage {...props} />}/>
 	        		<Route  render={() => <SplashPage {...props} /> }/>
 	        	</Switch>
